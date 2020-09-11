b8532cab4d8a22c9dece7bb308ec1d6654b297f1
Vector254 <ochrist7@gmail.com>
2020-09-11 13:04:21 +0300

    install and import flask

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    message="Welcome to the News App"
+    return render_template('index.html',message=message)
+
+@app.route('/top-headlines/<?country>')
+def movie(country):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('news.html',country=country)
